46257fe88fedaa0b2bb6166c3e4c564bf5052eeb
DaJuukes <dajuukes@gmail.com>
2020-11-06 15:18:07 -0800

    refactor to only express/html

---
+++ index.js
@@ -0,0 +1,24 @@
+// Requirements
+const express = require('express')
+const cors = require('cors')
+const ws = require('ws')
+
+require('dotenv').config({ path: './env/.env' })
+
+// Initialize webserver
+const app = express()
+app.use(cors())
+app.use(express.static('./public'))
+const server = app.listen(process.env.PORT)
+
+// Initialize websocket
+const wsServer = new ws.Server({ noServer: true })
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message))
+})
+
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, socket => {
+    wsServer.emit('connection', socket, request)
+  })
+})
